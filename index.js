600cb086329c643b858cc14746b11744297fcbc1
Adam <bosak3@gmail.com>
2021-10-22 18:11:46 -0500

    commit tree 8b1614fd3fd3303f6dd4b99e3e7db92bbe3764a4

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-    input.addEventListener('click', function() {
-        alert('I was clicked!');
-    });
+
+// const input = document.getElementById("input");
+// const mainClick = document.getElementById('main');
+// // const newDiv = document.createElement('div');
+
+// const addEventListener = ('click', () => {
+//     alert('I was clicked')
+// })
+
+
+// input.addEventListener('click', addEventListener);
+
+// function addEventListener (){
+
+//         alert('I was click!');
+    
+// }
+
+// mainClick.addEventListener('click', addEventListener)
+
+// document.body.append(newDiv);
+
+// newDiv.id = ('newNew')
+// newDiv.textContent = ('Click me!')
+
+// newDiv.addEventListener('click', addEventListener)
+
+function addingEventListener(){
+    const input = document.getElementById('input')
+    input.addEventListener('click', () => {
+        alert('I was clicked')
+    })
 }
 
 addingEventListener();
